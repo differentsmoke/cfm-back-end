e8a755eecb297064e96347de766b9e1c7656e5b5
differentsmoke <pablo.barria@post.harvard.edu>
2018-04-29 18:06:09 -0400

    trying specific CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     #origins 'differentsmoke.github.io'
-     origins '*'
+     origins 'differentsmoke.github.io'
+     #origins '*'
      resource '*',
        headers: :any,
        methods: [:get, :post, :put, :patch, :delete, :options, :head]
